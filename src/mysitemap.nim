3e70c9b99b37ee8a085e1ca5f943fc934a4de771
AbhishekKr <abhikumar163@gmail.com>
2023-01-01 04:55:00 +0530

    add to walk dynamic content dir for Markdown files for links
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- src/mysitemap.nim
+++ src/mysitemap.nim
@@ -22,12 +22,23 @@ proc main(rootURL, dynamicPath, mdPath, rootPath: string) =
     var urls = newSeq[XmlNode]()
     var path, name, ext: string
     let allowedExtensions = @[".html", ".htm"]
+
+    urls.add(url(rootURL, "1.00"))
+
     for f in walkDirRec(rootPath, {pcFile}, {pcDir}, true, true):
       (path, name, ext) = splitFile(f)
       if ext in allowedExtensions:
           var link = rootURL/"/"/f
           echo("adding path: ", link)
           urls.add(url(link, "1.00"))
+
+    for f in walkDir(mdPath, true, true):
+      (path, name, ext) = splitFile(f.path)
+      if ext == ".md":
+          var link = rootURL/dynamicPath/f.path
+          echo("adding path: ", link)
+          urls.add(url(link, "1.00"))
+
     let att = {
         "xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9",
         "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
@@ -39,7 +50,7 @@ proc main(rootURL, dynamicPath, mdPath, rootPath: string) =
 
 when isMainModule:
     if paramCount() < 3:
-      quit("wrong usage; e.g. mysitemap 'https://abhishekkr.github.io' '/blog.html#/' ./blogs [$PWD]")
+      quit("wrong usage; e.g. mysitemap 'https://abhishekkr.github.io' '/blog.html#/blogs' ./blogs [$PWD]")
     let rootURL = paramStr(1)
     let dynamicPath = paramStr(2)
     let mdPath = paramStr(3)
